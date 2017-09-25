f852160e60fd609605633a4fd1fa2c2af6c78d2a
Warper2001 <warper2001@gmail.com>
2017-09-25 14:03:00 -0500

    Update preinstall.sh

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,15 +1,15 @@
-# #!/bin/bash
+ #!/bin/bash
 
-# rm -rf lib
-# git clone -b SII_Variables_in_ec_slave_obj https://github.com/Warper2001/SOEM.git  lib
-# cd lib
-# #sed -i -- 's/STATIC/SHARED/g' CMakeLists.txt
-# sed -i '/target_link_libraries(soem\ ${OS_LIBS})/a \ \ set_property(TARGET soem PROPERTY POSITION_INDEPENDENT_CODE ON)' CMakeLists.txt
-# mkdir build
-# cd build
-# cmake ..
-# make
-# cd ..
-# #cp build/libsoem.so /usr/local/lib
-# cd ..
+ rm -rf lib
+ git clone -b SII_Variables_in_ec_slave_obj https://github.com/Warper2001/SOEM.git  lib
+ cd lib
+ #sed -i -- 's/STATIC/SHARED/g' CMakeLists.txt
+ sed -i '/target_link_libraries(soem\ ${OS_LIBS})/a \ \ set_property(TARGET soem PROPERTY POSITION_INDEPENDENT_CODE ON)' CMakeLists.txt
+ mkdir build
+ cd build
+ cmake ..
+ make
+ cd ..
+ #cp build/libsoem.so /usr/local/lib
+ cd ..
 
